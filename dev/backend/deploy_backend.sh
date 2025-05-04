d7c80799ef735f85956f992ff98649040d4b6626
Arthur Michel Tsimi <tarthurmichel@gmail.com>
2025-05-04 16:47:06 +0200

    implement symbolic link to access host container management system (docker or podman)

--- dev/backend/deploy_backend.sh
+++ dev/backend/deploy_backend.sh
@@ -9,23 +9,29 @@ clear='\033[0m'
 
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE}" )" >/dev/null 2>&1 && pwd )"
+ROOT_DIRECTORY="$SCRIPT_DIR/../.."
+CMS_PATH="$ROOT_DIRECTORY/cms" 
 BACKEND_IMAGE_NAME=localhost/javadevcontainer
 BACKEND_CONTAINER_NAME=javadevcontainer
 HOST_PORT=2222
 
+if [ ! -f $CMS_PATH ]; then
+    echo -e "${blue}Creating container management system (cms) symbolic link in $CMS_PATH ${clear}"
+    (ln -s $(which docker) $CMS_PATH) 2> /dev/null || (ln -s $(which podman) $CMS_PATH) || (echo "No docker nor podman detected exiting" && exit 1)
+fi
 
 echo -e "${blue}Building backend container...${clear}"
-podman build -f $SCRIPT_DIR/Dockerfile -t $BACKEND_IMAGE_NAME || exit 1
+$CMS_PATH build -f $SCRIPT_DIR/Dockerfile -t $BACKEND_IMAGE_NAME || exit 1
 echo -e "${green}OK${clear}"
 
 echo -e "${blue}Running container...${clear}"
-podman container run -d --rm -p $HOST_PORT:22 -p 8081:8081 --name $BACKEND_CONTAINER_NAME $BACKEND_IMAGE_NAME || exit 1
+$CMS_PATH container run -d --rm -p $HOST_PORT:22 -p 8081:8081 --name $BACKEND_CONTAINER_NAME $BACKEND_IMAGE_NAME || exit 1
 echo -e "${green}OK${clear}"
 
 echo -e "${blue}finalizing ssh configuration...${clear}"
-podman cp $HOME/.ssh/authorized_keys $BACKEND_CONTAINER_NAME:/home/dev/.ssh/authorized_keys || exit 1
+$CMS_PATH cp $HOME/.ssh/authorized_keys $BACKEND_CONTAINER_NAME:/home/dev/.ssh/authorized_keys || exit 1
 echo -e "${green}OK${clear}"
-podman container ls | grep -zP " javadevcontainer"
+$CMS_PATH container ls | grep -zP " javadevcontainer"
 
 echo -e "${green}Backend deployed${clear}"
 exit 0
